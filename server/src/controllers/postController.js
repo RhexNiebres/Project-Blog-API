715468b48622209c8f85d8ae473a332edd9a650c
RhexNiebres <rhexniebres@gmail.com>
2025-03-18 18:33:57 -0800

    feat: add createPost controller for creating post

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -1,34 +1,58 @@
-const {PrismaClient} = require('@prisma/client');
-const { request } = require('express');
+const { PrismaClient } = require("@prisma/client");
+const { request } = require("express");
 const prisma = new PrismaClient();
 
 //get all posts
 exports.getPosts = async (req, res) => {
-    try{
-        const posts = await prisma.post.findMany({
-            orderBy:{createdAt:'desc'},
-        });
-        res.json(posts);
-    }catch(error){
-        console.error('Error fetching posts:', error);
-        res.status(500).json({error:'Server error while fetching posts'});
-    }
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+    res.json(posts);
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    res.status(500).json({ error: "Server error while fetching posts" });
+  }
 };
 
-exports.getPostById = async(req,res)=>{
-    try{
-        const { id }= req.params;
+exports.getPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-        const post = await prisma.post.findUnique({
-            where:{id:parseInt(id)},
-        });
+    const post = await prisma.post.findUnique({
+      where: { id: parseInt(id) },
+    });
 
-        if(!post){
-            return res.status(404).json({error:'Post not Found'});
-        }
-        res.json(post);
-    }catch(error){
-        console.error('Error fetching post:', error);
-        res.status(500).json({error:'Server error while fetching post'})
+    if (!post) {
+      return res.status(404).json({ error: "Post not Found" });
     }
+    res.json(post);
+  } catch (error) {
+    console.error("Error fetching post:", error);
+    res.status(500).json({ error: "Server error while fetching post" });
+  }
 };
+
+exports.createPost = async (req, res) => {
+  try {
+    const { title, content, authorId } = req.body;
+
+    if (!title || !content || !authorId) {
+      return res
+        .status(400)
+        .json({ error: "Title,content and author are required" });
+    }
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId: parseInt(authorId),
+      },
+    });
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).json({ error: "server error while creating post" });
+  }
+};
+
