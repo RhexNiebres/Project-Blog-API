d5f09bfd2b6ee56a20c0ef94cae00d0b3331995e
RhexNiebres <rhexniebres@gmail.com>
2025-03-18 19:19:34 -0800

    feat(server): setup Express server with routing and dotenv configuration

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const indexRoutes = require("./routes/index")
+const authRoutes = require("./routes/auth")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", indexRoutes);
+app.use('/',authRoutes)
+
+app.listen(process.env.APP_PORT, () =>
+  console.log(`App listening on port ${process.env.APP_PORT}!`)
+);
