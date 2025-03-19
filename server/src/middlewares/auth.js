373b600cd6d2f9f096021743371794ab3581fa02
RhexNiebres <rhexniebres@gmail.com>
2025-03-18 19:25:38 -0800

    feat: add middleware to restrict access to authenticated users

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,8 @@
+module.exports.ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/login");
+  };
+  //a middleware that ensures that only logged-in users can access certain pages.
+  
