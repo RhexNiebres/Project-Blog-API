31816d6fe00b77aac011d844f75342c6acf8fe5f
RhexNiebres <rhexniebres@gmail.com>
2025-03-19 10:56:34 -0800

    feat: add get CommentById for getting comments by users

--- server/src/controllers/commentController.js
+++ server/src/controllers/commentController.js
@@ -1,16 +1,34 @@
-const { PrismaClient } = ('@prisma/client');
+const { PrismaClient } = "@prisma/client";
 const prisma = new PrismaClient();
 
-exports.getCommentByPost= async (req, res) => {
-    try{
-        const { postId }=req.params;
-        const comments = await prisma.comments.findMany({
-            where:{postId: parseInt(postId)},
-            orderBy:{createdAt: 'desc'},
-        });
-        res.json(comments);
-        }catch(error){
-            console.error('Error fetching comments:', error);
-            res.status(500).json({error:"Server error fetching comments"})
-        }
+exports.getCommentByPost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const comments = await prisma.comments.findMany({
+      where: { postId: parseInt(postId) },
+      orderBy: { createdAt: "desc" },
+    });
+    res.json(comments);
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    res.status(500).json({ error: "Server error fetching comments" });
+  }
 };
+
+exports.getCommentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comment = await prisma.comment.findUnique({
+      where: { postId: parseInt(id) },
+    });
+    if (!comments) {
+      console.error("Error comment does not exist", error);
+    }
+    res.json(comment);
+  } catch (error) {
+    console.error('Error fetching comment', error)
+    res.status(500).json({error:'Server error while fetching comment'})
+  }
+};
+
+
