e5727e8239bdf73e3bc00f1596ad2e2f99b899c1
RhexNiebres <rhexniebres@gmail.com>
2025-03-19 16:49:45 -0800

    feat: add routes for comments and posts

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,15 @@ const express = require("express");
 const app = express();
 const indexRoutes = require("./routes/index")
 const authRoutes = require("./routes/auth")
-
+const commentRoutes = require('./routes/comment')
+const postRoutes = require('./routes/post')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/", indexRoutes);
 app.use('/',authRoutes)
+app.use("/comments", commentRoutes);
+app.use("/posts", postRoutes);
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`App listening on port ${process.env.APP_PORT}!`)
