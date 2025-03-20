6ce2c905c7865bc8f1618b5b2cc4d67aeae0c5c4
RhexNiebres <rhexniebres@gmail.com>
2025-03-19 17:53:03 -0800

    update route for getting comments

--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/commentController");
 
-router.get("/:postId", commentController.getCommentsByPost);
+router.get("/comment/:id", commentController.getCommentById); 
+router.get("/:postId", commentController.getCommentByPost);
 router.post("/:postId", commentController.createComment);
 router.put("/:commentId", commentController.updateComment);
 router.delete("/:commentId", commentController.deleteComment);
