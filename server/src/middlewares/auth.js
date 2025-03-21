1c60cb2ca2c8f07faf2e67c8c16317f9b3f00850
RhexNiebres <rhexniebres@gmail.com>
2025-03-21 11:47:55 -0800

    feat : add jwt functions for generating token and verify token

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,8 +1,33 @@
-module.exports.ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/login");
-  };
-  //a middleware that ensures that only logged-in users can access certain pages.
-  
+require("dotenv").config({path:"../../.env"});
+const jwt = require("jsonwebtoken");
+ 
+function generateToken(user){
+  return jwt.sign({id: user.id, username: user.username,role:user.role },  process.env.JWT_SECRET,{
+    expiresIn:"24h"
+  });
+}
+
+function verifyToken(req, res, next){
+  const bearerHeader = req.headers["authorization"];
+
+  if(!bearerHeader){
+    return res.status(403).json({error: "Access forbidden. No token provided"});
+  }
+
+  const bearer = bearerHeader.split(" ");
+  const bearerToken = bearer[1];
+
+  if(bearerToken){
+    jwt.verify(bearerToken, process.env.JWT_SECRET,(err,decoded) => {
+      if(err){
+        return res.status(403).json({error: "Invalid token"});
+      }
+      req.user=decoded;
+      next()
+    });
+  }else{
+    return res.status(403).json({error: "Access forbidden. Token missing"})
+  }
+}
+
+module.exports ={generateToken, verifyToken}
