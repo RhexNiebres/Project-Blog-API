3335206aa3c059e771861b106c9441696408cffe
RhexNiebres <rhexniebres@gmail.com>
2025-03-26 12:20:50 -0800

    Removed unnecessary static file serving for React frontend since it will be hosted on Netlify.

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,23 @@
-require("dotenv").config({path:"../../.env"});
+require('dotenv').config();
 const express = require("express");
+const path = require("path");  
 const app = express();
-const indexRoutes = require("./routes/index")
-const authRoutes = require("./routes/auth")
-const commentRoutes = require('./routes/comment')
-const postRoutes = require('./routes/post')
+const indexRoutes = require("./routes/index");
+const authRoutes = require("./routes/auth");
+const commentRoutes = require("./routes/comment");
+const postRoutes = require("./routes/post");
+const cors = require("cors");// Allows the frontend (on a different domain) to communicate with the backend
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", indexRoutes);
-app.use("/",authRoutes)
 app.use("/comments", commentRoutes);
 app.use("/posts", postRoutes);
+app.use("/", indexRoutes);
+app.use("/", authRoutes);
+
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`App listening on port ${process.env.APP_PORT}!`)
