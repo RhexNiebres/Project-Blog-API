a3eb0f5bf08fab5d00bba02e48b4d57757df430d
RhexNiebres <rhexniebres@gmail.com>
2025-03-27 11:31:45 -0800

    add path to shared folder

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    preserveSymlinks: true, // shared dependencies
+    alias: {
+      "@shared": path.resolve(__dirname, "../shared"), // name for shared folder
+    },
+  },
   server: {
     proxy: {
       "/auth": "http://localhost:8080",
       "/comments": "http://localhost:8080",
       "/posts": "http://localhost:8080",
     },
-  },  
+  },
 });
