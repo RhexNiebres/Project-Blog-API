0165ba90b17ccbd70408b0fff3b8859b2a2647dc
RhexNiebres <rhexniebres@gmail.com>
2025-03-26 17:23:06 -0800

    Delete: remove unused routes

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-
-router.get("/sign-up", authController.getSignUp);
 router.post("/sign-up", authController.postSignUp);
 router.post("/log-in", authController.postLogin);
-router.get("/log-in", authController.getLogin);
-router.get("/log-out", authController.logout);
+router.post("/log-out", authController.logout);
 
 module.exports = router;
