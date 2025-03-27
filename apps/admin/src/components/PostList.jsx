348c866750736286806062972fa71430d331682e
RhexNiebres <rhexniebres@gmail.com>
2025-03-26 17:03:05 -0800

    Update: change dynamic  display of posts

--- apps/admin/src/components/PostList.jsx
+++ apps/admin/src/components/PostList.jsx
@@ -1,19 +1,16 @@
 import PostItem from "./PostItem";
 
 const PostList = ({ posts, onPostUpdated, onPostDeleted }) => {
-  return (
-    <div className="space-y-6">
-      <h2 className="text-3xl font-semibold text-blue-700">Existing Posts</h2>
-      {posts.map((post) => (
-        <PostItem
-          key={post.id} 
-          post={post} 
-          onPostUpdated={onPostUpdated} 
-          onPostDeleted={onPostDeleted}
-        />
-      ))}
-    </div>
-  );
-};
+    return (
+      <div>
+  {posts.map((post) => (
+    <PostItem key={post.id} post={post} onPostUpdated={onPostUpdated} onPostDeleted={onPostDeleted} />
+  ))}
+  {posts.length === 0 && <p>No posts available.</p>}
+</div>
 
-export default PostList;
+    );
+  };
+  
+  export default PostList;
+  
