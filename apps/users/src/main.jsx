02cc53b478e7369a30f7e7281ebf937b2a8fa902
RhexNiebres <rhexniebres@gmail.com>
2025-03-27 14:04:39 -0800

    update import paths

--- apps/users/src/main.jsx
+++ apps/users/src/main.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "@shared/pages/Login";
-import Signup from "@shared/pages/Signup";
-import LandingPage from "@shared/pages/LandingPage";
-import ProtectedRoute from "@shared/services/ProtectedRoute";
+import Login from "../../shared/pages/Login";
+import Signup from "../../shared/pages/Signup";
+import LandingPage from "../../shared/pages/LandingPage";
 import Home from "./pages/Home";
-import "./index.css";
+import ProtectedRoute from "../../shared/services/ProtectedRoute"; 
+import './index.css';  
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={< LandingPage/>} />
       <Route path="/login" element={<Login role="user" />} />
       <Route path="/signup" element={<Signup role="user" />} />
 
       <Route element={<ProtectedRoute />}>
-        <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<Home/>} />
       </Route>
+      
     </Routes>
   </BrowserRouter>
 );
