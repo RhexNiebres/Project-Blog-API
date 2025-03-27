69bbd3782b364cb11acac8d48ef9ad58ad2b65b1
RhexNiebres <rhexniebres@gmail.com>
2025-03-26 17:04:01 -0800

    Add ProtectedRoute component to handle authentication for protected routes

---
+++ apps/shared/services/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { isAuthenticated } from "../services/AuthService";
+
+const ProtectedRoute = () => {
+  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
