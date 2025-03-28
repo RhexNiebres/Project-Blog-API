85763c74a640ea6a5cd87e460461c3880555f1d4
RhexNiebres <rhexniebres@gmail.com>
2025-03-27 17:12:06 -0800

    update

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@shared": path.resolve(__dirname, "../shared"), // ✅ Use @shared to import files from shared/
-    },
-  },
-  optimizeDeps: {
-    include: ["react-router-dom"], // ✅ Make sure Vite processes react-router-dom
-  },
   server: {
-    watch: {
-      ignored: ["!../shared/**"], // ✅ Ensure Vite watches shared/ for changes
+    proxy: {
+      "/auth": "http://localhost:8080",
+      "/comments": "http://localhost:8080",
+      "/posts": "http://localhost:8080",
     },
-  },
+  },  
 });
