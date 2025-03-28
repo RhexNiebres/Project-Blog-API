d023b874b4e1e1df4fe03a0b715419e226bbde14
RhexNiebres <rhexniebres@gmail.com>
2025-03-27 18:30:10 -0800

    .

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,23 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/auth": "http://localhost:8080",
-      "/comments": "http://localhost:8080",
-      "/posts": "http://localhost:8080",
+  resolve: {
+    alias: {
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+      "react-router-dom": path.resolve("./node_modules/react-router-dom"),
     },
   },
   build: {
     rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom"], // Externalize all shared dependencies
-    },
-  },
-  resolve: {
-    alias: {
-      "@shared": "/apps/shared", // Adjust if needed
+      external: ["react", "react-dom", "react-router-dom"],
     },
   },
 });
