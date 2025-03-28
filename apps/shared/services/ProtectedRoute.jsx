ff5873a738bb7b80974e1b3ec61822e1a94adc84
RhexNiebres <rhexniebres@gmail.com>
2025-03-27 16:51:07 -0800

    fix auth import route

--- apps/shared/services/ProtectedRoute.jsx
+++ apps/shared/services/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
+import React from "react";  
 import { Navigate, Outlet } from "react-router-dom";
-import { isAuthenticated } from "../services/AuthService";
+import { isAuthenticated } from "../services/AuthService"; 
 
 const ProtectedRoute = () => {
   return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
