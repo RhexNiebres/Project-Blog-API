3d0e33d3cd8f7f2bbdad24686038d0e5e6ba8e61
RhexNiebres <rhexniebres@gmail.com>
2025-04-01 13:41:01 -0800

    remove build roleUp

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -11,11 +11,7 @@ export default defineConfig({
       "/posts": "http://localhost:8080",
     },
   },
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom"], 
-    },
-  },
+
   resolve: {
     alias: {
       "@shared": "/apps/shared", 
