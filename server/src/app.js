6be4003805abf923ce2fe8eea1e9922df1822d29
RhexNiebres <rhexniebres@gmail.com>
2025-04-04 18:17:13 +0800

    update allowed origin

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const indexRoutes = require("./routes/index");
 const authRoutes = require("./routes/auth");
 const commentRoutes = require("./routes/comment");
 const postRoutes = require("./routes/post");
-const cors = require("cors");// Allows the frontend (on a different domain) to communicate with the backend
+const cors = require("cors"); // Allows the frontend (on a different domain) to communicate with the backend
 
-app.use(cors({
-  origin: [`http://localhost:${process.env.APP_PORT}`, process.env.ADMIN_CLIENT_HOST, process.env.USERS_CLIENT_HOST], // Allow both local and deployed frontend
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true, // Allow cookies if needed
-}));
+const allowedOrigins = [
+  `http://localhost:${process.env.APP_PORT}`,
+  process.env.ADMIN_CLIENT_HOST,
+  process.env.USERS_CLIENT_HOST, 
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.options("*", cors());
 
@@ -22,7 +35,6 @@ app.use("/comments", commentRoutes);
 app.use("/posts", postRoutes);
 app.use("/", indexRoutes);
 app.use("/", authRoutes);
-
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`App listening on port ${process.env.APP_PORT}!`)
